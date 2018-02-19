967b42f7472b1314e4d1f7c5e173900cd2a114de
Yanikts <35463172+Yanikts@users.noreply.github.com>
2018-02-19 09:44:13 -0500

    Add files via upload
    Addition of the Arduino code to run the microcontroler (Adafruit Feather Huzzah) and decode a high-dynamic light sensor (Adafruit TSL2591) and send the values in OSC messages.

---
+++ HuzzahConnectSend.ino
@@ -0,0 +1,239 @@
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#else
+#include <WiFi.h>
+#endif
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_TSL2591.h"
+#include <Wire.h>
+
+char ssid[] = "VIDEOTRON7668"; //"Yanik's iPhone";          // your network SSID (name)
+char pass[] = "CKK4MR379CNK4"; //"allogilles";                    // your network password
+
+WiFiUDP Udp;                                // A UDP instance to let us send and receive packets over UDP
+const IPAddress outIp(192,168,0,182);        // remote IP of your computer
+const unsigned int outPort = 10000;          // remote port to receive OSC
+const unsigned int localPort = 8888;        // local port to listen for OSC packets (actually not used for sending)
+
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
+
+uint32_t lum, lux;
+uint16_t ir, full, visible;
+
+
+void setup() {
+    Serial.begin(115200);
+
+    // Connect to WiFi network
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("");
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("Starting UDP");
+    Udp.begin(localPort);
+    Serial.print("Local port: ");
+#ifdef ESP32
+    Serial.println(localPort);
+#else
+    Serial.println(Udp.localPort());
+#endif 
+
+    if (tsl.begin()) 
+    {
+      Serial.println(F("Found a TSL2591 sensor"));
+    } 
+    else 
+    {
+      Serial.println(F("No sensor found ... check your wiring?"));
+    while (1);
+    }
+
+    //displaySensorDetails();
+    configureSensor();
+    
+
+} //end of setup
+
+void loop() {
+
+    //simpleRead(); 
+    advancedRead();
+    //unifiedSensorAPIRead();
+    oscSend();
+
+    delay(10);
+    
+} //end of loop
+
+
+void displaySensorDetails(void){
+  sensor_t sensor;
+  tsl.getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" lux"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" lux"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution, 4); Serial.println(F(" lux"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  delay(500);
+  
+} // end of displaySensorDetails
+
+
+void configureSensor(void){
+  
+  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
+  //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+  //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
+  
+  // Changing the integration time gives you a longer time over which to sense light
+  // longer timelines are slower, but are good in very low light situtations!
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
+
+  /* Display the gain and integration time for reference sake */  
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Gain:         "));
+  tsl2591Gain_t gain = tsl.getGain();
+  switch(gain)
+  {
+    case TSL2591_GAIN_LOW:
+      Serial.println(F("1x (Low)"));
+      break;
+    case TSL2591_GAIN_MED:
+      Serial.println(F("25x (Medium)"));
+      break;
+    case TSL2591_GAIN_HIGH:
+      Serial.println(F("428x (High)"));
+      break;
+    case TSL2591_GAIN_MAX:
+      Serial.println(F("9876x (Max)"));
+      break;
+  }
+  Serial.print  (F("Timing:       "));
+  Serial.print((tsl.getTiming() + 1) * 100, DEC); 
+  Serial.println(F(" ms"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  
+} // end of configureSensor
+
+
+void simpleRead(void){
+  
+  // Simple data read example. Just read the infrared, fullspecrtrum diode 
+  // or 'visible' (difference between the two) channels.
+  // This can take 100-600 milliseconds! Uncomment whichever of the following you want to read
+  uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
+  //uint16_t x = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
+  //uint16_t x = tsl.getLuminosity(TSL2591_INFRARED);
+
+  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
+  Serial.print(F("Luminosity: "));
+  Serial.println(x, DEC);
+  
+} // end of simpleRead
+
+
+void advancedRead(void){
+  
+  // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
+  // That way you can do whatever math and comparisons you want!
+  lum = tsl.getFullLuminosity();
+  ir = lum >> 16;
+  full = lum & 0xFFFF;
+  visible = full - ir;
+  lux = tsl.calculateLux(full, ir);
+  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
+  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
+  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
+  Serial.print(F("Visible: ")); Serial.print(visible); Serial.print(F("  "));
+  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
+  
+} // end of advancedRead
+
+
+void unifiedSensorAPIRead(void){
+  
+  /* Get a new sensor event */ 
+  sensors_event_t event;
+  tsl.getEvent(&event);
+ 
+  /* Display the results (light is measured in lux) */
+  Serial.print(F("[ ")); Serial.print(event.timestamp); Serial.print(F(" ms ] "));
+  if ((event.light == 0) |
+      (event.light > 4294966000.0) | 
+      (event.light <-4294966000.0))
+  {
+    /* If event.light = 0 lux the sensor is probably saturated */
+    /* and no reliable data could be generated! */
+    /* if event.light is +/- 4294967040 there was a float over/underflow */
+    Serial.println(F("Invalid data (adjust gain or timing)"));
+  }
+  else
+  {
+    Serial.print(event.light); Serial.println(F(" lux"));
+  }
+  
+} // end of unifiedSensorAPIRead
+
+
+void oscSend(){
+
+  OSCMessage msgIR("/HUZ1/IR"); //creation des messages OSC pour ir, full, visible et lux
+  msgIR.add(ir);
+  OSCMessage msgFull("/HUZ1/Full");
+  msgFull.add(full);
+  OSCMessage msgVis("/HUZ1/Vis"); 
+  msgVis.add(visible);
+  OSCMessage msgLux("/HUZ1/Lux");
+  msgLux.add(lux);
+
+    
+  Udp.beginPacket(outIp, outPort); //envoi du message OSC contenant ir
+  msgIR.send(Udp);
+  Udp.endPacket();
+  
+  Udp.beginPacket(outIp, outPort); //envoi du message OSC contenant full
+  msgFull.send(Udp);
+  Udp.endPacket();
+
+  Udp.beginPacket(outIp, outPort);  //envoi du message OSC contenant lum
+  msgVis.send(Udp);
+  Udp.endPacket();
+
+  Udp.beginPacket(outIp, outPort);  //envoi du message OSC contenant lum
+  msgLux.send(Udp);
+  Udp.endPacket();
+  
+  
+  msgIR.empty(); //vidange des messages OSC
+  msgFull.empty();
+  msgVis.empty();
+  msgLux.empty();
+  
+  delay(100);
+
+} //end of oscSend
